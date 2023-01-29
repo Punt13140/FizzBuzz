69efeedc149219f95cda551dafb783cc9e928e60
NICOLAU Bastien <bastien.nicolau@univ-amu.fr>
2023-01-29 20:41:01 +0100

    First commit

---
+++ Fizzbuzz.php
@@ -0,0 +1,18 @@
+<?php
+
+function fizzbuzz(int $n): void
+{
+    for ($i = 1; $i <= $n; $i++) {
+        if ($i % 15 === 0) {
+            echo "FizzBuzz\n";
+        } elseif ($i % 3 === 0) {
+            echo "Fizz\n";
+        } elseif ($i % 5 === 0) {
+            echo "Buzz\n";
+        } else {
+            echo "$i\n";
+        }
+    }
+}
+
+fizzbuzz(100);
